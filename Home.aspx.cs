8dca0432cef2a76109174280c4de4c804299693c
kishan2566 <kparmar002@rku.ac.in>
2025-08-12 18:22:08 +0530

    Initial commit

---
+++ Home.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using System.Configuration;
+
+namespace car_rental_project
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        String s = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter da;
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            getcon();
+        }
+        void getcon()
+        {
+            con = new SqlConnection(s);
+            con.Open();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            
+            if (Button1.Text == "Book")
+            {
+                getcon();
+                
+                cmd = new SqlCommand("INSERT INTO bookings (car_name,pickup_location,dropoff_location,pickup_date,dropoff_date,pickup_time,dropoff_time) VALUES ('"+ddlcar.SelectedValue+"','"+txtpickup.Text+ "','"+txtdrop.Text+ "','"+txtDate1.Text+"','"+txtDate2.Text+"','"+ddltime1.SelectedValue+"','"+ddltime2.SelectedValue+"')", con);
+                cmd.ExecuteNonQuery();
+                
+            }
+        }
+    }
+}
---
+++ Login.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace car_rental_project
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        String s = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter da;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            getcon();
+        }
+        void getcon()
+        {
+            con = new SqlConnection(s);
+            con.Open();
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (Button1.Text == "Register")
+            {
+                getcon();
+
+                cmd = new SqlCommand("INSERT INTO Register (First_Name,Last_Name,Email,Password) VALUES ('" + First_Name.Text + "','" + Last_Name.Text + "','" + Email.Text + "','" + Password.Text + "')", con);
+                cmd.ExecuteNonQuery();
+
+            }
+        }
+    }
+}
